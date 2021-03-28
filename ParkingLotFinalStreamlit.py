02066a95b588e36f2f00852b3aec9462a906ba5e
SavageCoder777 <80233276+SavageCoder777@users.noreply.github.com>
2021-03-28 15:02:12 -0700

    Update ParkingLotFinalStreamlit.py

--- ParkingLotFinalStreamlit.py
+++ ParkingLotFinalStreamlit.py
@@ -5,6 +5,7 @@ import json
 import random
 import pandas as pd
 import os
+import time
 
 # python -m streamlit run ToDo.py
 
@@ -23,27 +24,20 @@ def opening_statements_READ():
     for action in infofileread:
         infofileaction = json.loads(action)
         try:
-            try:
-                if infofileaction["Reserve or Free"] == "Reserve":
-                    InformationTank[int(infofileaction["Spot"])] = infofileaction["Information"]
-                    ParkingLotlist[int(infofileaction["Spot"]) - 1] = 1
-                    global Revenue
-                    Revenue+=10
-            except:
-                continue
-            try:
-                if infofileaction["Reserve or Free"] == "Leave":
-                    InformationTank[int(infofileaction["Spot"])] = 0
-                    ParkingLotlist[int(infofileaction["Spot"]) - 1] = 0
-                    try:
-                        AccountInfo.append(infofileaction["Information"])
-                    except:
-                        continue
-            except:
-                continue
+            if infofileaction["Reserve or Free"] == "Reserve":
+                InformationTank[int(infofileaction["Spot"])] = infofileaction["Information"]
+                ParkingLotlist[int(infofileaction["Spot"]) - 1] = 1
+                global Revenue
+                Revenue+=10
+            if infofileaction["Reserve or Free"] == "Free":
+                InformationTank[int(infofileaction["Spot"])] = 0
+                ParkingLotlist[int(infofileaction["Spot"]) - 1] = 0
+                try:
+                    AccountInfo.append(infofileaction["Information"])
+                except:
+                    continue
         except:
             st.write("You are our first customer. Welcome to the Grand Opening of THE SMALLEST PARKING LOT.")
-    st.write("### Our current revenue is $" + str(Revenue) + " ... make it better.")
     infofileread.close()
 
 def check_spot_reservation_and_free():
@@ -61,42 +55,44 @@ def check_spot_reservation_and_free():
     st.write("Spot num. " + whichsinfo + " has been RESERVED " + str(Reserve) + " time/s and has been FREED " + str(Freed) + " time/s.")
     infofileread2.close()
 
-# def make_pie_chart(rof, rof2):
-#     Leave = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#     Leave2_0 = []
-#     colorchoices = ["darkgray", "gray", "lightgray", "lightblue", "lightgreen", "skyblue", "aqua", "red", "lightpink", "yellowgreen", "b", "g", "r", "c", "m", "y", "k", "tab:blue", "tab:orange", "tab:green", "tab:red", "tab:purple", "tab:brown", "tab:pink", "tab:gray", "tab:olive", "tab:cyan"]
-#     colors = []
-#     spots = []
-#     infofileread3 = open("d:\\Prahlad.V.C\\Documents\\Python\\Python Coding\\Other\\ParkingLot\\InformationFileJson.txt", "r")
-#     for action3 in infofileread3:
-#         infofileaction3 = json.loads(action3)
-#         if infofileaction3[rof] == rof2:
-#                 whichcolor = random.randint(0, 21)
-#                 Leave[int(infofileaction3["Spot"]) - 1]+=1
-#                 isrepeat = 0
-#                 for spot in range(len(spots)):
-#                     if spots[spot] != ("Spot " + str(infofileaction3["Spot"])):
-#                         isrepeat+=1
-#                 if isrepeat == len(spots):
-#                     spots.append("Spot " + str(infofileaction3["Spot"]))
-#                     colors.append(colorchoices[whichcolor])
-#         else:
-#             print("There is no data.")
-#             return
-#     for unnecessary in range(len(Leave)):
-#         if Leave[unnecessary] != 0:
-#             Leave2_0.append(Leave[unnecessary])
-#     plt.pie(Leave2_0, labels = spots, colors = colors, startangle = 90, shadow = True, radius = 1.2, autopct = '%1.1f%%') 
-#     plt.legend()
-#     plt.show()
-#     infofileread3.close()
+def make_pie_chart(rof):
+    Leave = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    Leave2_0 = []
+    colorchoices = ["darkgray", "gray", "lightgray", "lightblue", "lightgreen", "skyblue", "aqua", "red", "lightpink", "yellowgreen", "b", "g", "r", "c", "m", "y", "k", "tab:blue", "tab:orange", "tab:green", "tab:red", "tab:purple", "tab:brown", "tab:pink", "tab:gray", "tab:olive", "tab:cyan"]
+    colors = []
+    spots = []
+    infofileread3 = open("d:\\Prahlad.V.C\\Documents\\Python\\Python Coding\\Other\\ParkingLot\\Streamlit.json", "r")
+    try:
+        for action3 in infofileread3:
+            infofileaction3 = json.loads(action3)
+            if infofileaction3["Reserve or Free"] == rof:
+                whichcolor = random.randint(0, 21)
+                Leave[int(infofileaction3["Spot"]) - 1]+=1
+                isrepeat = 0
+                for spot in range(len(spots)):
+                    if spots[spot] != ("Spot " + str(infofileaction3["Spot"])):
+                        isrepeat+=1
+                if isrepeat == len(spots):
+                    spots.append("Spot " + str(infofileaction3["Spot"]))
+                    colors.append(colorchoices[whichcolor])
+    except:
+        st.write("There is no data.")
+        return
+    for unnecessary in range(len(Leave)):
+        if Leave[unnecessary] != 0:
+            Leave2_0.append(Leave[unnecessary])
+    # plt.pie(Leave2_0, labels = spots, colors = colors, startangle = 90, shadow = True, radius = 1.2, autopct = '%1.1f%%') 
+    fig, ax = plt.subplots()
+    ax.pie(Leave2_0, labels = spots)
+    st.pyplot(fig)
+    infofileread3.close()
 
-# def make_graphs():
-#     questioninputgraph = input("               Do you want a graph which shows the (1) Reserved /or/ (2) Freed /spots? - ")
-#     if questioninputgraph == "1":
-#         make_pie_chart("Reserve", "Reserve")
-#     elif questioninputgraph == "2":
-#         make_pie_chart("Leave", "Leave")
+def make_graphs():
+    questioninputgraph = st.selectbox("Do You want to See a Chart which Shows the Reserved or Freed Spots?", ["", "Reserved", "Freed"], key = "Which Pie")
+    if "Reserved" in questioninputgraph:
+        make_pie_chart("Reserve")
+    elif "Freed" in questioninputgraph:
+        make_pie_chart("Free")
 
 def make_spot_graph():
     ParkingLotlistduplicate = ParkingLotlist
@@ -142,10 +138,9 @@ def load_data():
         with open(stfile):
             pass
 
-def reserve_spot_INSERT():
+def reserve_spot_INSERT(uoa, whatuseroption):
     whichspot = st.text_input("Which spot do you want?")
     place = (int(whichspot) - 1)
-    print("Which Spot: ", place)
     if ParkingLotlist[place] == 0:
         name = st.text_input("Name")
         licenseplate = st.text_input("License Plate")
@@ -153,8 +148,15 @@ def reserve_spot_INSERT():
         color = st.text_input("Car Color")
         if st.button("Add Reservation", key = "Add Reservation"):
             add_reservation(name, licenseplate, car, color, int(whichspot))
-        ld = load_data()
-        st.write(ld)
+            name = ""
+            licenseplate = ""
+            car = ""
+            color = ""
+            whichspot = ""
+            whatuseroption = ""
+            uoa = ""
+            st.write("Reservation Successfully Added!")
+        ParkingLotlist[int(whichspot)] = 1
         InformationTank[int(whichspot)] = (name, licenseplate, car, color)
     else:
         st.write("I'm sorry, but that spot is full. Please try again.")
@@ -163,13 +165,13 @@ def check_spot_info_READ():
     questioninputinfo = st.text_input("Which spot do you want to check in on?")
     checkinfo = int(questioninputinfo)
     if ParkingLotlist[(checkinfo - 1)] == 0:
-        st.write("                    I'm sorry, but I'm afraid that there are no car/s in that spot.")
+        st.write("I'm sorry, but I'm afraid that there are no car/s in that spot.")
     else:
-        st.write("                    Name: " + InformationTank[checkinfo]["Name"])
-        st.write("                    License Plate: " + InformationTank[checkinfo]["License Plate"])
-        st.write("                    Car Type: " + InformationTank[checkinfo]["Car Type"])
-        st.write("                    Color: " + InformationTank[checkinfo]["Color"])
-        st.write("                    Money Due: $10 [Ten Dollars]")
+        st.write("Name: " + InformationTank[checkinfo]["Name"])
+        st.write("License Plate: " + InformationTank[checkinfo]["License Plate"])
+        st.write("Car Type: " + InformationTank[checkinfo]["Car Type"])
+        st.write("Color: " + InformationTank[checkinfo]["Car Color"])
+        st.write("Money Due: $10 [Ten Dollars]")
 
 def remove_reservation(cardtype, cardname, cardcvv, cardexpiry, whichspot2):
     time = datetime.datetime.now()
@@ -190,7 +192,7 @@ def remove_reservation(cardtype, cardname, cardcvv, cardexpiry, whichspot2):
         json.dump(leaveinfo, appendstfile)
         appendstfile.write("\n")
 
-def delete_spot_info_DELETE():
+def delete_spot_info_DELETE(uoa, whatuseroption):
     carretrieval = st.text_input("What spot is your car in?")
     retrieval = int(carretrieval) - 1
     if ParkingLotlist[retrieval] == 0:
@@ -201,6 +203,9 @@ def delete_spot_info_DELETE():
             if st.button("Retrieve Car", key = "Retrieve Car via Cash"):
                 remove_reservation("N/A", "N/A", "N/A", "N/A", int(carretrieval))
                 st.write("Thank you for parking at The Smallest Parking Lot!  Hope to see you soon!")
+                time.sleep(0.5)
+                whatuseroption = ""
+                uoa = ""
         elif "Card" in coc:
             cardtype = st.text_input("Card Type")
             cardname = st.text_input("Card Name")
@@ -209,26 +214,33 @@ def delete_spot_info_DELETE():
             if st.button("Retrieve Car", key = "Retrieve Car via Card"):
                 remove_reservation(cardtype, cardname, cardcvv, cardexpiry, int(carretrieval))
                 st.write("Thank you for parking at The Smallest Parking Lot!  Hope to see you soon!")
-        ld2 = load_data()
-        st.write(ld2)
+                time.sleep(0.5)
+                cardtype = ""
+                cardname = ""
+                cardcvv = ""
+                cardexpiry = ""
+                whatuseroption = ""
+                uoa = ""
         ParkingLotlist[int(carretrieval)] = 0
         InformationTank[int(carretrieval)] = 0
 
 opening_statements_READ()
 uoa = st.selectbox("Are you a user or an admin?", ["", "User", "Admin"], key = "uoa")
 if "User" in uoa:
+    st.write("### Our fee is only $10 [Ten Dollars]! You're free to come!")
     whatuseroption = st.selectbox("What do you want to do?", ["", "Reserve a Spot", "Retrieve a Car", "Show Spots"], key = "whatuseroption")
     if "Reserve a Spot" in whatuseroption:
-        reserve_spot_INSERT()
+        reserve_spot_INSERT(uoa, whatuseroption)
 
     elif "Retrieve a Car" in whatuseroption:
-        delete_spot_info_DELETE()
+        delete_spot_info_DELETE(uoa, whatuseroption)
 
     elif "Show Spots" in whatuseroption:
         make_spot_graph()
 
 if "Admin" in uoa:
-    whatadminoption = st.selectbox("What do you want to do?", ["", "Check Spot Information", "Check Full Spots", "Check Empty Spots", "Check Num. of Times a Spot has been Reserved and Freed", "See Chart Detailing Spot Preferences"], key = "whatadminoption")
+    st.write("### Our current revenue is $" + str(Revenue) + " ... make it better.")
+    whatadminoption = st.selectbox("What do you want to do?", ["", "Check Spot Information", "Check Full Spots", "Check Empty Spots", "Check Num. of Times a Spot has been Reserved and Freed", "See Chart Detailing Spot Preferences", "See Complete Spot Information"], key = "whatadminoption")
     if "Check Spot Information" in whatadminoption:
         check_spot_info_READ()
 
@@ -254,5 +266,9 @@ if "Admin" in uoa:
         check_spot_reservation_and_free()
 
     elif "See Chart Detailing Spot Preferences" in whatadminoption:
-        # make_graph()
-        st.write("Function Not Available Now.")
+        make_graphs()
+        # st.write("Function Not Available Now.")
+    
+    elif "See Complete Spot Information" in whatadminoption:
+        ld = load_data()
+        st.write(ld)
